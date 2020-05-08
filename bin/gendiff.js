a6460af73e0b32fabfee71e2f02558f7e1097172
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-08 12:47:58 +0300

    Add mandatory launch arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ import commander from 'commander';
 const program = new commander.Command();
 program
   .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
   .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
