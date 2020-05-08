6c2ce97006e90bd7108791f7274da130a23e7c8f
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-08 18:36:54 +0300

    Implement grabbing filepaths from console

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import commander from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new commander.Command();
 program
@@ -7,6 +8,11 @@ program
   .helpOption('-h, --help', 'output usage information')
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(filepath1, filepath2);
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
 program.parse(process.argv);
