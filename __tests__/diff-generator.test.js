dd21d1a9bc3951c1ca0b2e324ccc6100da1ac2b0
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-09 15:15:43 +0300

    Implement tests for ini files diff generation
    Tests were unified, now all flat fixtures have the same
    input and output values, so expected diff is the same
    for all formats

--- __tests__/diff-generator.test.js
+++ __tests__/diff-generator.test.js
@@ -5,25 +5,11 @@ import genDiff from '../src/diff-generator';
 const getAbsoluteFixturePath = (filename) => path.join(path.resolve(), '__fixtures__', filename);
 const getRelativeFixturePath = (filename) => path.join('.', '__fixtures__', filename);
 
-test('generate json diff', () => {
-  const relativePathLeft = getRelativeFixturePath('flat_left.json');
-  const absolutePathRight = getAbsoluteFixturePath('flat_right.json');
-
-
-  const expectedDiff = `host: hexlet.io
-- timeout: 50
-+ timeout: 20
-- proxy: 123.234.53.22
-- follow: false
-+ verbose: true`;
-
-  const generatedDiff = genDiff(relativePathLeft, absolutePathRight);
-  expect(generatedDiff).toEqual(expectedDiff);
-});
-
-test('generate yaml diff', () => {
-  const relativePathLeft = getRelativeFixturePath('flat_left.yml');
-  const absolutePathRight = getAbsoluteFixturePath('flat_right.yaml');
+test.each([
+  ['json', getRelativeFixturePath('flat_left.json'), getAbsoluteFixturePath('flat_right.json')],
+  ['yaml', getRelativeFixturePath('flat_left.yml'), getAbsoluteFixturePath('flat_right.yaml')],
+  ['ini', getAbsoluteFixturePath('flat_left.ini'), getRelativeFixturePath('flat_right.ini')],
+])('generate %s diff', (formatTitle, pathLeft, pathRight) => {
   const expectedDiff = `home: hexlet.io
 - age: 25
 + age: 20
@@ -32,6 +18,6 @@ test('generate yaml diff', () => {
 + admin: true
 + expired: false`;
 
-  const generatedDiff = genDiff(relativePathLeft, absolutePathRight);
+  const generatedDiff = genDiff(pathLeft, pathRight);
   expect(generatedDiff).toEqual(expectedDiff);
 });
