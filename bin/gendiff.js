12a35743dd9331db5ad3f43c434dbd10d7ee7c6c
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-11 10:06:59 +0300

    Add stylish formatter as default for CLI utility

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,11 +7,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(filepath1, filepath2);
-    const diff = genDiff(filepath1, filepath2);
+    const diff = genDiff(filepath1, filepath2, program.format);
     console.log(diff);
   });
 
