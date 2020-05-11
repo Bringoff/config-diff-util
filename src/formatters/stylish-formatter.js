2c70e64812f9e32f87ad8baaa49b6b4e9be64b8a
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-11 09:58:37 +0300

    Remove code duplication from stylish formatter

--- src/formatters/stylish-formatter.js
+++ src/formatters/stylish-formatter.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
 
+const buildPadding = (depth, operationSign = ' ') => `${' '.repeat(4 * depth - 2)}${operationSign} `;
+
 const formatValue = (value, depth) => {
   if (!_.isObject(value)) return `${value}`;
   return [
     '{',
-    ...Object.entries(value).map(([k, v]) => `${' '.repeat(4 * (depth + 1))}${k}: ${v}`),
-    `${' '.repeat(4 * depth)}}`,
+    // eslint-disable-next-line no-use-before-define
+    ...Object.entries(value).map(([k, v]) => `${formatKeyValueLine(k, v, depth + 1)}`),
+    `${buildPadding(depth)}}`,
   ].join('\n');
+};
+
+const formatKeyValueLine = (key, value, depth, operationSign) => {
+  const paddingAndOperationSign = buildPadding(depth, operationSign);
+  return `${paddingAndOperationSign}${key}: ${formatValue(value, depth)}`;
 };
 
 const formatDiff = (diffAst, depth = 0) => {
@@ -14,14 +22,15 @@ const formatDiff = (diffAst, depth = 0) => {
     return diffAst.map((entry) => formatDiff(entry, depth + 1)).join('\n');
   }
   const formattingOptions = {
-    unchanged: ({ key, value }) => `${' '.repeat(4 * depth)}${key}: ${formatValue(value, depth)}`,
-    added: ({ key, value }) => `${' '.repeat(4 * depth - 2)}+ ${key}: ${formatValue(value, depth)}`,
-    removed: ({ key, value }) => `${' '.repeat(4 * depth - 2)}- ${key}: ${formatValue(value, depth)}`,
-    modified: ({ key, value, oldValue }) => `${' '.repeat(4 * depth - 2)}+ ${key}: ${formatValue(value, depth)}\n${' '.repeat(4 * depth - 2)}- ${key}: ${formatValue(oldValue, depth)}`,
+    unchanged: ({ key, value }) => `${formatKeyValueLine(key, value, depth)}`,
+    added: ({ key, value }) => `${formatKeyValueLine(key, value, depth, '+')}`,
+    removed: ({ key, value }) => `${formatKeyValueLine(key, value, depth, '-')}`,
+    modified: ({ key, value, oldValue }) => `${formatKeyValueLine(key, value, depth, '+')}
+${formatKeyValueLine(key, oldValue, depth, '-')}`,
     nestedModified: ({ key, value }) => [
-      `${' '.repeat(4 * depth)}${key}: {`,
+      `${buildPadding(depth)}${key}: {`,
       `${formatDiff(value, depth)}`,
-      `${' '.repeat(4 * depth)}}`,
+      `${buildPadding(depth)}}`,
     ].join('\n'),
   };
 
