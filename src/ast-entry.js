767e063284ec9d5c3291792ed777fcfb2f8b2060
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-11 10:35:23 +0300

    Fix Entry to be a void function That's an expected function structure to be used with the `new` keyword

--- src/ast-entry.js
+++ src/ast-entry.js
@@ -1,5 +1,6 @@
 export default function Entry(event, key, value, oldValue) {
-  return {
-    event, key, value, oldValue,
-  };
+  this.event = event;
+  this.key = key;
+  this.value = value;
+  this.oldValue = oldValue;
 }
