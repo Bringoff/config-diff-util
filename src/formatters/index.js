87738cfc8637d655f789ad09aaf2d2e45684e882
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-11 22:02:19 +0300

    Add json formatter implementation

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import stylish from './stylish-formatter.js';
 import plain from './plain-formatter.js';
+import json from './json-formatter.js';
 
 export default (format) => {
   const formatters = {
     stylish,
     plain,
+    json,
   };
   return formatters[format];
 };
