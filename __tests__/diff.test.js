d38fb4eb1f522504d7128d3413f9c3010c94e718
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-11 18:35:56 +0300

    Add plain formatter test cases

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -6,15 +6,18 @@ import genDiff from '../src/diff-generator.js';
 const getAbsoluteFixturePath = (filename) => path.join(path.resolve(), '__fixtures__', filename);
 const getRelativeFixturePath = (filename) => path.join('.', '__fixtures__', filename);
 
-describe('diff building', () => {
+describe.each([
+  ['stylish'],
+  ['plain'],
+])('diff building', (formatterName) => {
   test.each([
     ['json', getRelativeFixturePath('before.json'), getAbsoluteFixturePath('after.json')],
     ['yaml', getRelativeFixturePath('before.yml'), getAbsoluteFixturePath('after.yml')],
     ['ini', getRelativeFixturePath('before.ini'), getAbsoluteFixturePath('after.ini')],
-  ])('generate %s diff', async (_formatTitle, pathLeft, pathRight) => {
-    const expectedDiff = await fs.readFile(getRelativeFixturePath('diff.stylish'), 'utf-8');
+  ])('generate %s diff', async (_extension, pathLeft, pathRight) => {
+    const expectedDiff = await fs.readFile(getRelativeFixturePath(`diff.${formatterName}`), 'utf-8');
 
-    const generatedDiff = genDiff(pathLeft, pathRight, 'stylish');
+    const generatedDiff = genDiff(pathLeft, pathRight, formatterName);
     expect(generatedDiff).toEqual(expectedDiff);
   });
 });
