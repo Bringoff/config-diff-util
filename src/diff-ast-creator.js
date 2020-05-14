c8f3250b01c8bffba02ce8f4fbc278aa2d6eb288
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-14 10:28:31 +0300

    Simplify ast build by extracting common keys first

--- src/diff-ast-creator.js
+++ src/diff-ast-creator.js
@@ -3,26 +3,27 @@ import _ from 'lodash';
 const isNestedStructure = (value) => _.isObject(value) && !_.isArray(value);
 
 const buildDiffAst = (before, after) => {
-  const beforeEntries = Object.entries(before);
-  const afterAddedEntries = Object.entries(after)
-    .filter(([key]) => !_.has(before, key));
+  const beforeKeys = Object.keys(before);
+  const afterKeys = Object.keys(after);
 
-  const commonDiffEvents = beforeEntries.map(([key, value]) => {
-    if (!_.has(after, key)) {
-      return { event: 'removed', key, value };
-    }
+  const commonKeys = _.intersection(beforeKeys, afterKeys);
+  const commonDiffEvents = commonKeys.map((key) => {
+    const beforeValue = before[key];
     const afterValue = after[key];
-    if (value === afterValue) return { event: 'unchanged', key, value };
-    if (isNestedStructure(value) && isNestedStructure(afterValue)) {
-      return { event: 'nestedModified', key, value: buildDiffAst(value, afterValue || {}) };
+    if (beforeValue === afterValue) return { event: 'unchanged', key, value: beforeValue };
+    if (isNestedStructure(beforeValue) && isNestedStructure(afterValue)) {
+      return { event: 'nestedModified', key, value: buildDiffAst(beforeValue, afterValue) };
     }
     return {
-      event: 'modified', key, value: afterValue, oldValue: value,
+      event: 'modified', key, value: afterValue, oldValue: beforeValue,
     };
   });
+  const removedEvents = _.difference(beforeKeys, commonKeys)
+    .map((key) => ({ event: 'removed', key, value: before[key] }));
+  const addedEvents = _.difference(afterKeys, commonKeys)
+    .map((key) => ({ event: 'added', key, value: after[key] }));
 
-  const afterAddedEvents = afterAddedEntries.map(([key, value]) => ({ event: 'added', key, value }));
-  return _.sortBy([...commonDiffEvents.flat(), ...afterAddedEvents], 'key');
+  return _.sortBy([...commonDiffEvents.flat(), ...removedEvents, ...addedEvents], 'key');
 };
 
 export default buildDiffAst;
