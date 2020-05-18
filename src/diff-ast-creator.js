9687e4f321848f47cc2af0af5067134a6a52d4a6
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-18 10:39:18 +0300

    Optimize ast creation by using arrays intersection

--- src/diff-ast-creator.js
+++ src/diff-ast-creator.js
@@ -6,10 +6,13 @@ const buildDiffAst = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
 
-  const commonKeys = _.intersection(keys1, keys2);
-  const commonDiffEvents = commonKeys.map((key) => {
+  const allKeys = _.union(keys1, keys2);
+  const commonDiffEvents = allKeys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
+
+    if (!keys1.includes(key)) return { type: 'added', key, value: value2 };
+    if (!keys2.includes(key)) return { type: 'removed', key, value: value1 };
     if (value1 === value2) return { type: 'unchanged', key, value: value1 };
     if (isNestedStructure(value1) && isNestedStructure(value2)) {
       return { type: 'nestedModified', key, value: buildDiffAst(value1, value2) };
@@ -18,12 +21,7 @@ const buildDiffAst = (data1, data2) => {
       type: 'modified', key, value: value2, oldValue: value1,
     };
   });
-  const removedEvents = _.difference(keys1, commonKeys)
-    .map((key) => ({ type: 'removed', key, value: data1[key] }));
-  const addedEvents = _.difference(keys2, commonKeys)
-    .map((key) => ({ type: 'added', key, value: data2[key] }));
-
-  return _.sortBy([...commonDiffEvents.flat(), ...removedEvents, ...addedEvents], 'key');
+  return _.sortBy([...commonDiffEvents.flat()], 'key');
 };
 
 export default buildDiffAst;
