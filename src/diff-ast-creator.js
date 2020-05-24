58ccbd5c875022f3ae52e88d8319fd9bd653025c
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-24 12:57:15 +0300

    Improve speed by using `has` instead of `includes`

--- src/diff-ast-creator.js
+++ src/diff-ast-creator.js
@@ -9,8 +9,8 @@ const buildDiffAst = (data1, data2) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
-    if (!keys1.includes(key)) return { type: 'added', key, value: value2 };
-    if (!keys2.includes(key)) return { type: 'removed', key, value: value1 };
+    if (!_.has(data1, key)) return { type: 'added', key, value: value2 };
+    if (!_.has(data2, key)) return { type: 'removed', key, value: value1 };
     if (value1 === value2) return { type: 'unchanged', key, value: value1 };
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       return { type: 'nestedModified', key, value: buildDiffAst(value1, value2) };
