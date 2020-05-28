71eafab8cb402c8b95fbd8a9959beeadcc503417
Artem Kalachyan <ua.bringoff@gmail.com>
2020-05-28 10:10:29 +0300

    Sort keys before creating AST

--- src/diff-ast-creator.js
+++ src/diff-ast-creator.js
@@ -4,7 +4,7 @@ const buildDiffAst = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
 
-  const allKeys = _.union(keys1, keys2);
+  const allKeys = _.union(keys1, keys2).sort();
   const commonDiffEvents = allKeys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
@@ -19,7 +19,7 @@ const buildDiffAst = (data1, data2) => {
       type: 'modified', key, value: value2, oldValue: value1,
     };
   });
-  return _.sortBy(commonDiffEvents, 'key');
+  return commonDiffEvents;
 };
 
 export default buildDiffAst;
